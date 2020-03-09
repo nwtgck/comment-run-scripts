2d1b2468717cbe9743d16ac55aae1609ebe2cccc
Ryo Ota <nwtgck@nwtgck.org>
2020-03-09 18:25:05 +0900

    feat: create top page

--- build-scripts.bash
+++ build-scripts.bash
@@ -9,3 +9,12 @@ npm ci
 npm run all
 cd ..
 cp hello-world-comment/dist/index.js $DIST_DIR/hello-world-comment.js
+
+# Create index.html for visitors to top page
+cd $DIST_DIR
+echo "<ul>" > "index.html"
+for fname in $(find . -type f); do
+  echo "  <li><a href="$fname">$fname</a></li>" >> "index.html"
+done
+echo "</ul>" >> "index.html"
+cd ..
